fffa81a8e562163a1ccb5c16cc38bdc437fc5690
[Narmin Bagirova] <[narminnbagirova@gmail.com]>
2022-12-29 19:03:58 +0400

    Add project files.

---
+++ algorithm task 1.1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace algorithm_task_1._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int month, year;
+            if (number >= 1 && number <= 1188)
+            {
+                year = number / 12;
+                month = number % 12;
+                Console.WriteLine($"{year} il, {month} ay");
+            }
+        }
+    }
+}
